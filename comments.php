c32251842da016123b6a3e0841e519b7a1f7d29a
aearns <aearns@gmail.com>
2022-05-20 18:05:35 +0000

    first commit

---
+++ comments.php
@@ -0,0 +1,14 @@
+<?php
+
+# This is a sinlge line comment
+
+// This is a also single line comment
+
+/* This is a multiple line comment
+line 2
+*/
+echo "Hello World\n\n";  #This line prints out "Hello World"
+
+echo "This is a new line";
+
+?>
---
+++ hello.php
@@ -0,0 +1,5 @@
+<?php
+echo "<h1> Hello world </h1>";
+echo "<h4> This is a new line <h4>";
+echo "Welcome";
+?>
